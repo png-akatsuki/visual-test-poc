4878094a2488c7b6b690ad496f9d01386b4c39e2
Tanea Joshi <tanea.joshi@siemens.com>
2024-09-25 15:02:42 +0530

    test

--- src/components/ProfileCard.stories.ts
+++ src/components/ProfileCard.stories.ts
@@ -22,41 +22,41 @@ const meta = {
 } satisfies Meta<typeof ProfileCard>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+// type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  parameters: {
-    percy: {
-      name: 'Profile Pic Default',
-      skip: false, // Snapshot this story
-      // waitForSelector: '.profile-pic', Example for Wait for profile-pic element before snapshot
-      queryParams: {
-        theme: 'light', // Add custom query parameters for snapshot
-      },
-      additionalSnapshots: [
-        { suffix: ' with globals', globals: { textDirection: 'rtl' } },
-      ],
-    },
-  },
-  args: {
-    defaultShowMore: false, // Default collapsed state
-  },
-};
+// export const Default: Story = {
+//   parameters: {
+//     percy: {
+//       name: 'Profile Pic Default',
+//       skip: false, // Snapshot this story
+//       // waitForSelector: '.profile-pic', Example for Wait for profile-pic element before snapshot
+//       queryParams: {
+//         theme: 'light', // Add custom query parameters for snapshot
+//       },
+//       additionalSnapshots: [
+//         { suffix: ' with globals', globals: { textDirection: 'rtl' } },
+//       ],
+//     },
+//   },
+//   args: {
+//     defaultShowMore: false, // Default collapsed state
+//   },
+// };
 
-export const Expanded: Story = {
-  parameters: {
-    percy: {
-      name: 'Profile Pic Expanded',
-      skip: false, // Ensure this snapshot is not skipped
-      queryParams: {
-        theme: 'dark', // Apply dark theme via query params
-      },
-      additionalSnapshots: [
-        { suffix: ' with globals', globals: { textDirection: 'rtl' } },
-      ],
-    },
-  },
-  args: {
-    defaultShowMore: true, // Expanded state with 'See More' clicked
-  },
-};
+// export const Expanded: Story = {
+//   parameters: {
+//     percy: {
+//       name: 'Profile Pic Expanded',
+//       skip: false, // Ensure this snapshot is not skipped
+//       queryParams: {
+//         theme: 'dark', // Apply dark theme via query params
+//       },
+//       additionalSnapshots: [
+//         { suffix: ' with globals', globals: { textDirection: 'rtl' } },
+//       ],
+//     },
+//   },
+//   args: {
+//     defaultShowMore: true, // Expanded state with 'See More' clicked
+//   },
+// };
